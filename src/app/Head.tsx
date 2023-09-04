285144d0a3cbf2136a5049760d48fc5ff48cd73a
junggyoo <junggyoo.1@gmail.com>
2023-09-04 21:27:41 +0900

    feat: 메인 페이지 SEO 최적화

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -1,9 +1,14 @@
 export default function Head() {
   return (
     <head>
-      <title>Pokemon</title>
+      <title>포켓몬 도감</title>
       <meta charSet="utf-8" />
-      <meta name="description" content="Pokemon" />
+      <meta name="description" content="포켓몬 도감 정보를 제공합니다." />
+      <meta name="author" content="eric" />
+      <meta
+        name="keywords"
+        content="포켓몬, 도감, 포켓몬 도감, Studiomate, 스튜디오메이트"
+      />
       <link rel="icon" href="/favicon.ico" />
     </head>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
 import MainBody from '@/views/main/MainBody';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '포켓몬 도감',
+  openGraph: {
+    description: '포켓몬 도감 정보를 제공합니다.',
+    url: 'http://localhost:3000',
+    locale: 'ko_KR',
+    siteName: 'studiomate-pokemon',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+        width: 300,
+        height: 300,
+      },
+    ],
+  },
+  keywords: [
+    '포켓몬',
+    '포켓몬 도감',
+    '포켓몬 정보',
+    '포켓몬 진화',
+    '포켓몬 도감 정보',
+    '포켓몬 도감 진화',
+  ],
+};
+
 export default function MainPage() {
   return <MainBody />;
 }
